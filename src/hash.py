3ff5b18b27a90e3becba4958158405fc31daf134
mikekh84 <mikekh@uw.edu>
2016-10-07 18:35:50 -0700

    Removed rand from ._hash. Changed get to go to hash index. Hash not adds ord of char.

--- src/hash.py
+++ src/hash.py
@@ -11,18 +11,29 @@ class Hash(object):
 
     def _hash(self, key):
         """hash the key provided"""
-        hash = len(key) * randint(0, 9) % self.size
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        hash = hash % self.size
         return hash
 
     def get(self, key):
         """takes key returns assoseated value"""
-        for item in self.table:
-            for tup in item:
-                if tup[0] == key:
-                    return tup[1]
+        hashed = self._hash(key)
+        for tup in self.table[hashed]:
+            if tup[0] == key:
+                return tup[1]
 
     def set(self, key, val):
         """takes key and value and stores them in the table"""
         pair = (key, val)
         slot = self._hash(key)
         self.table[slot].append(pair)
+
+    # def _hash_fnv(self, key):
+    #     """Use FNV to hash key."""
+    #     hash = 2166136261
+    #     for char in key:
+    #         hash = (hash * 16777619) ^ ord(char)
+    #     return hash % self.size
+    

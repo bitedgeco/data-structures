3e7abe190149dce0b015e0ed98c6508b576c6ac5
mikekh84 <mikekh@uw.edu>
2016-10-08 18:25:56 -0700

    Added a test the reads from a word list.

--- src/test_hash.py
+++ src/test_hash.py
@@ -1,5 +1,6 @@
 from hash import Hash
 
+WORDS = '/mnt/c/py401d/words'
 
 def test_set_and_get():
     """put something in the hash table then make sure its there"""
@@ -12,3 +13,10 @@ def test_fnv_hash():
     table = Hash(10)
     table.set_fnv('test', 'test2')
     assert table.get_fnv('test') == 'test2'
+
+def test_words_additive():
+    table = Hash(1000)
+    for word in open(WORDS):
+        table.set(word, word)
+    for word in open(WORDS):
+        assert table.get(word) == word
